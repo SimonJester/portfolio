0f6913f09b61acef4e464f81558f19982f98697e
SimonJester <joelc.usa@gmail.com>
2014-08-16 17:54:44 -0600

    Make ticker symbols lower case

---
+++ getPortfolio.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+"""
+Retrieves current list of ticker symbols that are in portfolio.
+All symbols are in lower-case.
+"""
+
+#TODO: Get portfolio list from a text file.
+#TODO: Handle command line parms.
+
+import sys
+
+
+def get_portfolio():
+    """Return a list of ticker symbols for entire portfolio"""
+    # Currently this list is hardcoded to match my spreadsheet layout
+    portfolio = [
+         'BTC'
+        ,'LTC'
+        ,'ETH'
+        ,''
+        ,''
+        ,'ZRC'
+        ,'NMC'
+        ,'MSC'
+        ,'ANC'
+        ,'NXT'
+        ,'XCP'
+        ,''
+        ,''
+        ,'PTS'
+        ,'BTSX'
+        ,''
+        ,''
+        ,'XPM'
+        ,'PPC'
+        ,'FTC'
+        ,'SWARMPRE'
+        ,'DRK'
+        ,'MAID'
+        ,'TOR'
+        ,''
+        ,''
+        ,'DOGE'
+        ,'MEC'
+        ,'QRK'
+        ,'XRP'
+        ]
+
+    # Convert to lowercase
+    for index, ticker in enumerate(portfolio):
+        portfolio[index] = ticker.lower()
+
+    return portfolio
+
+
+def main():
+    """Parse command line options (TODO)"""
+    print get_portfolio()
+
+
+if __name__ == "__main__":
+    main()
+
